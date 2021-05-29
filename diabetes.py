e89f90f42e189eca6ae328efc629ad25767bfe08
Andres Mendoza <afma102831@gmail.com>
2021-05-29 16:55:10 -0500

    taller banco finalizado

--- diabetes.py
+++ diabetes.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat May 29 14:08:05 2021
+Created on Sat May 29 14:35:35 2021
 
 @author: Verde
 """
@@ -28,68 +28,62 @@ simplefilter(action='ignore', category=ConvergenceWarning)
 url = ''
 data = pd.read_csv(url)
 
-#ahora remplazaremos todos los valores que hay por datos numericos
-data.job.replace(['management', 'technician', 'entrepreneur', 'blue-collar', 'unknown', 'retired', 'admin.', 'services', 
-                  'self-employed', 'unemployed', 'housemaid', 'student'], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], inplace=True)
+#Reemplazar los valores que son NaN
+data.Glucose.replace(0, 121, inplace=True)
 
-data.marital.replace(['married', 'single', 'divorced'], [0, 1, 2], inplace=True)
+data.BloodPressure.replace(0, 69, inplace=True)
 
-data.education.replace(['tertiary', 'secondary', 'unknown', 'primary'], [0, 1, 2, 3], inplace=True)
+data.SkinThickness.replace(0, 21, inplace=True)
 
-data.default.replace(['no', 'yes'], [0, 1], inplace=True)
+data.Insulin.replace(0, 80, inplace=True)
 
-data.housing.replace(['no', 'yes'], [0, 1], inplace=True)
-
-data.loan.replace(['no', 'yes'], [0, 1], inplace=True)
-
-data.contact.replace(['unknown', 'cellular', 'telephone'], [0, 1, 2], inplace=True)
-
-data.month.replace(['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'],
-                   [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], inplace=True)
-
-data.poutcome.replace(['failure', 'success', 'other', 'unknown'], [0, 1, 2, 3], inplace=True)
-
-data.y.replace(['no', 'yes'], [0, 1], inplace=True)
+data.BMI.replace(0, 31, inplace=True)
 
 #confirmamos que todo este correcto
 data.info()
 
-##Empezamos hacer los rangos para grupar la data
-# Definimos los rangos de balance
-rangosBalance = [-8020, -2000, 0, 1000, 2000, 4000, 10000, 20000, 105000]
-nombresBalance = ['1', '2', '3', '4', '5', '6', '7', '8']
-data.balance = pd.cut(data.balance, rangosBalance, labels=nombresBalance)
+##definimos los rangos para agrupar la data
+#definimos los rangos de grosor de la piel
+rangosPiel = [0, 10, 15, 20, 25, 30, 35, 40, 100]
+nombresPiel = ['1', '2', '3', '4', '5', '6', '7', '8']
+data.SkinThickness = pd.cut(data.SkinThickness, rangosPiel, labels=nombresPiel)
 
-#Definimos los rangos de campaña
-rangosCampaña = [0, 1, 2, 3, 4, 5, 10, 70]
-nombresCampaña = ['1', '2', '3', '4', '5', '6', '7']
-data.campaign = pd.cut(data.campaign, rangosCampaña, labels=nombresCampaña)
+#definimos los rangos de glucosa
+rangosGlucosa = [40, 80, 100, 120, 140, 160, 200]
+nombresGlucosa = ['1', '2', '3', '4', '5', '6']
+data.Glucose = pd.cut(data.Glucose, rangosGlucosa, labels=nombresGlucosa)
 
-#Definimos los rangos dia
-rangosDia = [0, 5, 10, 15, 20, 25, 30, 40]
-nombresDia = ['1', '2', '3', '4', '5', '6', '7']
-data.day = pd.cut(data.day, rangosDia, labels=nombresDia)
+#definimos los rangos de presión sanguinea
+rangosPresion = [20, 50, 60, 65, 70, 75, 80, 90, 100, 125]
+nombresPresion = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+data.BloodPressure = pd.cut(data.BloodPressure, rangosPresion, labels=nombresPresion)
 
-#Definimos los rangos de duracion
-rangosDuracion = [-1, 50, 100, 150, 200, 250, 400, 600, 1000, 5000]
-nombresDuracion = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-data.duration = pd.cut(data.duration, rangosDuracion, labels=nombresDuracion)
+#definimos los rangos de insulina
+rangosInsulina = [10, 75, 80, 100, 150, 200, 900]
+nombresInsulina = ['1', '2', '3', '4', '5', '6']
+data.Insulin = pd.cut(data.Insulin, rangosInsulina, labels=nombresInsulina)
 
-#Definimos los rangos  de edad
-rangosEdad = [10, 25, 30, 40, 50, 60, 100]
-nombresEdad = ['1', '2', '3', '4', '5', '6']
-data.age = pd.cut(data.age, rangosEdad, labels=nombresEdad)
+#definimos los rangos de IMC
+rangosIMC = [0, 18.5, 25, 27, 35, 40, 50, 70]
+nombresIMC = ['1', '2', '3', '4', '5', '6', '7']
+data.BMI = pd.cut(data.BMI, rangosIMC, labels=nombresIMC)
 
-#Definimos los rangos de PDays
-rangosPDays = [-2, -1, 50, 100, 150, 250, 400, 900]
-nombresPDays = ['1', '2', '3', '4', '5', '6', '7']
-data.pdays = pd.cut(data.pdays, rangosPDays, labels=nombresPDays)
+#definimos los rangos de edad
+rangosEdad = [20, 30, 40, 50, 60, 70, 80, 90]
+nombresEdad = ['1', '2', '3', '4', '5', '6', '7']
+data.Age = pd.cut(data.Age, rangosEdad, labels=nombresEdad)
 
-#Definimos los rangos de previo
-rangosPrevio = [-1, 0, 10, 300]
-nombresPrevio = ['1', '2', '3']
-data.previous = pd.cut(data.previous, rangosPrevio, labels=nombresPrevio)
+#definimos los rangos de dia
+rangosDia = [0, 0.5, 1, 1.5, 2, 2.5]
+nombresDia = ['1', '2', '3', '4', '5']
+data.DiabetesPedigreeFunction = pd.cut(data.DiabetesPedigreeFunction, rangosDia, labels=nombresDia)
 
+##Entrenamiento y validación cruzada
+# 0 = No tiene diabetes y 1 = tiene diabetes
+x = np.array(data.drop(['Outcome'], 1))
+y = np.array(data.Outcome)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 ## 2. Utilizar Cross validation para entrenar y probar los modelos con mínimo 10 splits
 #Definimos el método de entrenamiento.
@@ -146,14 +140,7 @@ def plot_roc_curve(fpr, tpr, label):
     plt.show()
 
 
-##Entrenamiento y validación cruzada
-# 0 = No y 1 = Si
-x = np.array(data.drop(['y'], 1))
-y = np.array(data.y)
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
-
-## 1. Entrenar 5 modelos con distintos algoritmos de Machine Learning
+# 1. Entrenar 5 modelos con distintos algoritmos de Machine Learning
 #Modelo Regresión Logistica
 modelRL, acc_validationRL, acc_testRL, y_predRL = entrenamiento(LogisticRegression(), x_train, x_test, y_train, y_test)
 matriz_confusionRL, AUCRL, fprRL, tprRL = matriz_confusion_auc(modelRL, x_test, y_test, y_predRL)
@@ -173,6 +160,7 @@ matriz_confusionVC, AUCVC, fprVC, tprVC = matriz_confusion_auc(modelVC, x_test, 
 #Modelo Arbol de Decisión
 modelAD, acc_validationAD, acc_testAD, y_predAD = entrenamiento(DecisionTreeClassifier(), x_train, x_test, y_train, y_test)
 matriz_confusionAD, AUCAD, fprAD, tprAD = matriz_confusion_auc(modelAD, x_test, y_test, y_predAD)
+
 
 
 ## 3. Realizar usando la librería Pandas una tabla que resuma los resultados de las métricas de cada modelo. En las filas debe ir el nombre del
@@ -205,27 +193,26 @@ table = {'Métricas': ['Regresión Logística', 'Gausiano', 'Perceptrón Multica
 print(pd.DataFrame(table))
 
 
-## 4. Imprimir la matriz de confusión de cada modelo usando la librería pandas
+## 4. Imprimir la matriz de confusión de cada modelo usando la librería pandas  
 #Regresión logistica
 print("Matriz de confusión regresión logistica:")
 print(confusion_matrix( y_test, y_predRL))
-mostrar_resultados(y_test, y_predRL)
+
 #Gausiano
 print("Matriz de confusión gausiano:")
 print(confusion_matrix( y_test, y_predGA))
-mostrar_resultados(y_test, y_predGA)
+
 #Perceptrón Multicapa
 print("Matriz de confusión perceptrón multicapa:")
 print(confusion_matrix( y_test, y_predPM))
-mostrar_resultados(y_test, y_predPM)
+
 #Vecinos Cercanos
 print("Matriz de confusión vecinos cercanos:")
 print(confusion_matrix( y_test, y_predVC))
-mostrar_resultados(y_test, y_predVC)
+
 #Árbol de Decisión
 print("Matriz de confusión árbol de decisión:")
 print(confusion_matrix( y_test, y_predAD))
-mostrar_resultados(y_test, y_predAD)
 
 
 # 5. Imprimir las 5 matrices de confusión en un solo gráfico, empleando el mapa de calor de la librería Seaborn
@@ -237,15 +224,15 @@ ax3 = fig.add_subplot(3, 3, 3)
 ax4 = fig.add_subplot(3, 3, 4)
 ax5 = fig.add_subplot(3, 3, 5)
 
-sns.heatmap(matriz_confusionRL, ax=ax1, cmap="YlOrRd", annot=True)
-sns.heatmap(matriz_confusionGA, ax=ax2, cmap="YlOrRd", annot=True)
-sns.heatmap(matriz_confusionPM, ax=ax3, cmap="YlOrRd", annot=True)
-sns.heatmap(matriz_confusionVC, ax=ax4, cmap="YlOrRd", annot=True)
-sns.heatmap(matriz_confusionAD, ax=ax5, cmap="YlOrRd", annot=True)
+sns.heatmap(matriz_confusionRL, ax=ax1, cmap="YlGnBu", annot=True)
+sns.heatmap(matriz_confusionGA, ax=ax2, cmap="YlGnBu", annot=True)
+sns.heatmap(matriz_confusionPM, ax=ax3, cmap="YlGnBu", annot=True)
+sns.heatmap(matriz_confusionVC, ax=ax4, cmap="YlGnBu", annot=True)
+sns.heatmap(matriz_confusionAD, ax=ax5, cmap="YlGnBu", annot=True)
 plt.show()
 
-
-##6. Imprimir las métricas de precisión, recall y f1 de cada clase de cada modelo
+## 6. Imprimir las métricas de precisión, recall y f1 de cada clase de cada modelo
+#Regresión Logistica
 print("Métricas regresión logistica: ")
 mostrar_resultados(y_test, y_predRL)
 
@@ -266,38 +253,46 @@ print("Métricas árbol de decisión: ")
 mostrar_resultados(y_test, y_predAD)
 
 
-# 7. Mostrar las 5 curvas de ROC en el mismo gráfico 
+# 7. Mostrar las 5 curvas de ROC en el mismo gráfico
 plt.figure(figsize=(25, 10))
 
 #Regresion Logistica
 probRL = modelRL.predict_proba(x_test)
 probRL = probRL[:, 1]
+auc = roc_auc_score(y_test, probRL)
 fprRL, tprRL, _ = roc_curve(y_test, probRL)
-plt.plot(fprRL, tprRL, label='ROC regresión logistica')
+plot_roc_curve(fprRL, tprRL, label='ROC regresión logistica')
+
 
 #Gausiano
 probGA = modelGA.predict_proba(x_test)
 probGA = probGA[:, 1]
+auc = roc_auc_score(y_test, probGA)
 fprGA, tprGA, _ = roc_curve(y_test, probGA)
-plt.plot(fprGA, tprGA, label='ROC gausiano')
+plot_roc_curve(fprGA, tprGA, label='ROC gausiano')
 
 #Perceptron Multicapa
 probPM = modelPM.predict_proba(x_test)
 probPM = probPM[:, 1]
+auc = roc_auc_score(y_test, probPM)
 fprPM, tprPM, _ = roc_curve(y_test, probPM)
-plt.plot(fprPM, tprPM, label='ROC preceptrón multicapa')
+plot_roc_curve(fprPM, tprPM, label='ROC preceptrón multicapa')
+
 
 #Vecinos Cercanos
 probVC = modelVC.predict_proba(x_test)
 probVC = probVC[:, 1]
+auc = roc_auc_score(y_test, probVC)
 fprVC, tprVC, _ = roc_curve(y_test, probVC)
-plt.plot(fprVC, tprVC, label='ROC vecinos cercanos')
+plot_roc_curve(fprVC, tprVC, label='ROC vecinos cercanos')
 
 #Árbol de Decision
 probAD = modelAD.predict_proba(x_test)
 probAD = probAD[:, 1]
+auc = roc_auc_score(y_test, probAD)
 fprAD, tprAD, _ = roc_curve(y_test, probAD)
-plt.plot(fprAD, tprAD, label='ROC árbol de decisión')
+plot_roc_curve(fprAD, tprAD, label='ROC árbol de decisión')
+
 
 plt.plot([0, 1], [0, 1], color='black', label='Línea de NO discriminacion')
 plt.xlabel('Tasa de falsos positivos')
@@ -305,4 +300,3 @@ plt.ylabel('Tasa de verdaderos positivos')
 plt.title('Curvas de ROC')
 plt.legend()
 plt.show()
-
